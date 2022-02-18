76e27f4259dd1416a09b13f99c9389db24bf381e
Chuck McCallum <mccalluc@users.noreply.github.com>
2022-02-18 14:06:44 -0500

    test that we get expected errors (#17)
    * add test coverage
    
    * spell out flags
    
    * Add ignores, and ratchet fail-under to 100
    
    * add bad fixtures
    
    * Add another fixture
    
    * linting, move fixtures, and another bad fixture
    
    * Add fixture to cover code branch

--- src/builders/anndata_builders.py
+++ src/builders/anndata_builders.py
@@ -25,7 +25,7 @@ class RNASeqAnnDataZarrViewConfBuilder(ViewConfBuilder):
         zarr_path = 'hubmap_ui/anndata-zarr/secondary_analysis.zarr'
         file_paths_found = [file["rel_path"] for file in self._entity["files"]]
         # Use .zgroup file as proxy for whether or not the zarr store is present.
-        if f'{zarr_path}/.zgroup' not in file_paths_found:  # pragma: no cover
+        if f'{zarr_path}/.zgroup' not in file_paths_found:
             message = f'RNA-seq assay with uuid {self._uuid} has no matching .zarr store'
             raise FileNotFoundError(message)
         vc = VitessceConfig(name=self._uuid)
@@ -36,9 +36,10 @@ class RNASeqAnnDataZarrViewConfBuilder(ViewConfBuilder):
         # or AnnData default (like X_umap or X).
         cell_set_obs = ["leiden"]
         cell_set_obs_names = ["Leiden"]
-        dags = [dag
-                for dag in self._entity['metadata']['dag_provenance_list'] if 'name' in dag]
-        if(any(['azimuth-annotate' in dag['origin'] for dag in dags])):  # pragma: no cover
+        dags = [
+            dag for dag in self._entity['metadata']['dag_provenance_list']
+            if 'name' in dag]
+        if(any(['azimuth-annotate' in dag['origin'] for dag in dags])):
             request_init = self._get_request_init() or {}
             headers = request_init.get('headers', {})
             response = requests.get(
