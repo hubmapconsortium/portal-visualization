64c56d1eb61df4e94fb77a122ddae50d6c94978b
mccalluc <mccalluc@users.noreply.github.com>
2022-03-11 17:36:30 -0500

    handle whatever comes at us

--- src/utils.py
+++ src/utils.py
@@ -25,16 +25,44 @@ def group_by_file_name(files):
     return [list(g) for _, g in groupby(sorted_files, _get_path_name)]
 
 
-def get_conf_cells(vc, md):
-    if not hasattr(vc, 'to_python'):
-        vc = VitessceConfig.from_dict(vc)
-    imports, conf_code = vc.to_python()
+def get_conf_cells(vc_anything, md):
     cells = [
-        nbformat.v4.new_markdown_cell(md),
+        nbformat.v4.new_markdown_cell(md)
+    ] + _get_cells_from_anything(vc_anything)
+    conf = (
+        vc_anything.to_dict()
+        if hasattr(vc_anything, 'to_dict')
+        else vc_anything
+    )
+    return ConfCells(conf, cells)
+
+
+def _get_cells_from_anything(vc):
+    if isinstance(vc, dict):
+        return _get_cells_from_dict(vc)
+    if isinstance(vc, list):
+        return _get_cells_from_list(vc)
+    if hasattr(vc, 'to_python'):
+        return _get_cells_from_obj(vc)
+    raise Exception(f'Viewconf is unexpected type {type(vc)}')
+
+
+def _get_cells_from_list(vc_list):
+    cells = [nbformat.v4.new_markdown_cell('Multiple visualizations are available.')]
+    for vc in vc_list:
+        cells.extend(_get_cells_from_anything(vc))
+    return cells
+
+
+def _get_cells_from_dict(vc_dict):
+    vc_obj = VitessceConfig.from_dict(vc_dict)
+    return _get_cells_from_obj(vc_obj)
+
+
+def _get_cells_from_obj(vc_obj):
+    imports, conf_expression = vc_obj.to_python()
+    return [
         nbformat.v4.new_code_cell(f'from vitessce import {", ".join(imports)}'),
-        nbformat.v4.new_code_cell(f'conf = {conf_code}'),
-        nbformat.v4.new_code_cell(f'conf.widget()'),
+        nbformat.v4.new_code_cell(f'conf = {conf_expression}\nconf.widget()'),
     ]
-    # notebook = nbformat.v4.new_notebook(cells=cells)
-    # nbformat.writes(notebook)
-    return ConfCells(vc.to_dict(), cells)
+
