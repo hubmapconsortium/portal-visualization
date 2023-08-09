9a94352e2a0e06b7538855cf5b3cd72d94525e1e
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-08-09 12:44:08 -0400

    fix `replace_url_in_file` test

--- src/portal_visualization/builders/base_builders.py
+++ src/portal_visualization/builders/base_builders.py
@@ -47,9 +47,9 @@ class ViewConfBuilder(ABC):
         ...     'rel_path': 'cells.json',
         ...     'coordination_values': { 'obsType': 'cell' } }
         >>> pprint(builder._replace_url_in_file(file))
-        >>> { 'file_type': 'cells.json',
-        ...   'url': 'https://example.com/uuid/cells.json?token=groups_token'}
-        ...   'coordination_values': {'obsType': 'cell'} }
+        {'coordination_values': {'obsType': 'cell'},
+         'file_type': 'cells.json',
+         'url': 'https://example.com/uuid/cells.json?token=groups_token'}
         """
 
         return {
