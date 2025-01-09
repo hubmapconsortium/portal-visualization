b637a26facafcbb4fffa1fc2b74b74c1bfa35aa6
Tabassum Kakar <tabassum_kakar@hms.harvard.edu>
2025-01-09 09:17:34 -0500

    Using globals for view-types

--- src/portal_visualization/builders/imaging_builders.py
+++ src/portal_visualization/builders/imaging_builders.py
@@ -18,6 +18,9 @@ from ..paths import (IMAGE_PYRAMID_DIR, OFFSETS_DIR, SEQFISH_HYB_CYCLE_REGEX,
                      SEGMENTATION_SUBDIR)
 from .base_builders import ViewConfBuilder
 
+BASE_IMAGE_VIEW_TYPE = 'image'
+SEG_IMAGE_VIEW_TYPE = 'seg'
+KAGGLE_IMAGE_VIEW_TYPE = 'kaggle-seg'
 
 class AbstractImagingViewConfBuilder(ViewConfBuilder):
     def __init__(self, entity, groups_token, assets_endpoint, **kwargs):
@@ -96,7 +99,7 @@ class AbstractImagingViewConfBuilder(ViewConfBuilder):
             )
 
     def _setup_view_config(self, vc, dataset, view_type, disable_3d=[], use_full_resolution=[]):
-        if view_type == "image":
+        if view_type == BASE_IMAGE_VIEW_TYPE:
             vc.add_view(cm.SPATIAL, dataset=dataset, x=3, y=0, w=9, h=12).set_props(
                 useFullResolutionImage=use_full_resolution
             )
@@ -112,10 +115,12 @@ class AbstractImagingViewConfBuilder(ViewConfBuilder):
                 disable3d=disable_3d, disableChannelsIfRgbDetected=True
             )
             # Adding the segmentation mask on top of the image
-            if view_type == 'kaggle_seg':
+            if view_type == KAGGLE_IMAGE_VIEW_TYPE:
+                # vc.link_views_by_dict([spatial_view, lc_view])
+                # TODO: The image-channel view disappears after the following
                 vc.link_views_by_dict([spatial_view, lc_view], {
-                'imageLayer': CL([{'photometricInterpretation': 'RGB', }]),
-            }, meta=True, scope_prefix=get_initial_coordination_scope_prefix("A", "image"))
+                    'imageLayer': CL([{'photometricInterpretation': 'RGB', }]),
+                }, meta=True, scope_prefix=get_initial_coordination_scope_prefix("A", "image"))
 
         return vc
 
@@ -135,9 +140,8 @@ class AbstractImagingViewConfBuilder(ViewConfBuilder):
             dataset = dataset.add_object(
                 ImageOmeTiffWrapper(img_url=img_url, offsets_url=offsets_url, name=Path(found_images[0]).name)
             )
-            if self.view_type == 'kaggle-seg':
+            if self.view_type == KAGGLE_IMAGE_VIEW_TYPE:
                 self._add_segmentation_image(dataset)
-                
                 
         else:
             images = [
@@ -151,7 +155,7 @@ class AbstractImagingViewConfBuilder(ViewConfBuilder):
                 MultiImageWrapper(images, use_physical_size_scaling=self.use_physical_size_scaling)
             )
         conf = self._setup_view_config(vc, dataset, self.view_type, use_full_resolution=self.use_full_resolution).to_dict()
-        if "image" in self.view_type:
+        if self.view_type == BASE_IMAGE_VIEW_TYPE:
             del conf["datasets"][0]["files"][0]["options"]["renderLayers"]
         return get_conf_cells(conf)
 
@@ -164,7 +168,7 @@ class ImagePyramidViewConfBuilder(AbstractImagingViewConfBuilder):
     def __init__(self, entity, groups_token, assets_endpoint, **kwargs):
         super().__init__(entity, groups_token, assets_endpoint, **kwargs)
         self.image_pyramid_regex = IMAGE_PYRAMID_DIR
-        self.view_type = "image"
+        self.view_type = BASE_IMAGE_VIEW_TYPE
 
     def get_conf_cells(self, **kwargs):
         return self.get_conf_cells_common(self._get_img_and_offset_url, **kwargs)
@@ -178,7 +182,7 @@ class SegImagePyramidViewConfBuilder(AbstractImagingViewConfBuilder):
     def __init__(self, entity, groups_token, assets_endpoint, **kwargs):
         super().__init__(entity, groups_token, assets_endpoint, **kwargs)
         self.image_pyramid_regex = f"{SEGMENTATION_SUBDIR}/{IMAGE_PYRAMID_DIR}/{SEGMENTATION_SUPPORT_IMAGE_SUBDIR}"
-        self.view_type = "seg"
+        self.view_type = SEG_IMAGE_VIEW_TYPE
 
     def get_conf_cells(self, **kwargs):
         return self.get_conf_cells_common(self._get_img_and_offset_url_seg, **kwargs)
@@ -189,7 +193,7 @@ class KaggleSegImagePyramidViewConfBuilder(AbstractImagingViewConfBuilder):
         super().__init__(entity, groups_token, assets_endpoint, **kwargs)
         self.image_pyramid_regex = f"{IMAGE_PYRAMID_DIR}/{SEGMENTATION_SUPPORT_IMAGE_SUBDIR}"
         self.seg_image_pyramid_regex = IMAGE_PYRAMID_DIR
-        self.view_type = "kaggle-seg"
+        self.view_type = KAGGLE_IMAGE_VIEW_TYPE
 
     def get_conf_cells(self, **kwargs):
         return self.get_conf_cells_common(self._get_img_and_offset_url_seg, **kwargs)
