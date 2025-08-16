c05c8bd34c8ebd4e2a6df51e65f68d2dc159590e
Tabassum Kakar <tkakar@wpi.edu>
2025-08-16 19:02:56 -0400

    Linting

--- test/test_builders.py
+++ test/test_builders.py
@@ -131,10 +131,10 @@ def mock_zarr_store(entity_path, mocker):
             group_names.append("predicted_label")
         elif is_pan_azimuth:
             group_names = ["leiden_wnn", "leiden_rna",
-                "final_level_labels",
-                "full_hierarchical_labels",
-                "CL_Label",
-            ]
+                           "final_level_labels",
+                           "full_hierarchical_labels",
+                           "CL_Label",
+                           ]
         groups = obs.create_groups(*group_names)
         for group in groups:
             group["categories"] = zarr.array(["0", "1", "2"])
